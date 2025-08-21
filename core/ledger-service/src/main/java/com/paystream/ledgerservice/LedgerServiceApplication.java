b4a5e584c0862c9baba26f3bfd0650bbe8c61728
Kayahan Güneri <kayahanguneri8@gmail.com>
2025-08-21 18:02:20 +0300

    fix ledger

--- core/ledger-service/src/main/java/com/paystream/ledgerservice/LedgerServiceApplication.java
+++ core/ledger-service/src/main/java/com/paystream/ledgerservice/LedgerServiceApplication.java
@@ -2,15 +2,10 @@ package com.paystream.ledgerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
-@EnableScheduling // Enables @Scheduled tasks such as the OutboxRelay
-
-
-@SpringBootApplication
-
+@SpringBootApplication          // use only once
+@EnableScheduling               // keep this for OutboxRelay
 public class LedgerServiceApplication {
   public static void main(String[] args) {
     SpringApplication.run(LedgerServiceApplication.class, args);
